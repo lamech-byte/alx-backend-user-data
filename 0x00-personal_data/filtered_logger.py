518b1d8ab4696001b4727a95de35a3bdd42ee323
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-02 15:50:53 +0300

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,17 @@
+To implement the `main` function that retrieves data from the database and displays it in a filtered format,
+you can update the `filtered_logger.py` file as follows:
+
+```python
 #!/usr/bin/env python3
 """
 filtered_logger.py - Module for filtering log data using regex.
 """
 
+
 import os
 import re
 import logging
 import mysql.connector
-
 
 def filter_datum(fields, redaction, message, separator):
     """
@@ -32,15 +36,10 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record):
         log_msg = super().format(record)
-        return filter_datum(
-            self.fields, self.REDACTION, log_msg, self.SEPARATOR
-        )
+        return filter_datum(self.fields, self.REDACTION, log_msg, self.SEPARATOR)
 
 # Define the PII_FIELDS constant containing the fields considered PII.
-
-
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
-
 
 def get_logger():
     """ Return a logger object with specific settings """
@@ -55,7 +54,6 @@ def get_logger():
     logger.propagate = False
 
     return logger
-
 
 def get_db():
     """ Return a connector to the database """
@@ -72,3 +70,33 @@ def get_db():
     )
 
     return db
+
+def main():
+    """ Retrieve and display data from the users table """
+    logger = get_logger()
+    db = get_db()
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        logger.info(
+            "name={}; email={}; phone={}; ssn={}; password={}; ip={}; last_login={}; user_agent={};".format(*row)
+        )
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
+```
+
+With this implementation, the `main` function retrieves all rows from the "users" table, and for each row,
+it logs the filtered data using the `get_logger` function with the defined `RedactingFormatter`.
+
+Now, when you run `./filtered_logger.py` with the appropriate environment variables set, it should connect to
+the database, retrieve the data, and display it in the filtered format as expected:
+
+```
+[HOLBERTON] user_data INFO 2019-11-19 18:37:59,596: name=***; email=***; phone=***; ssn=***; password=***; ip=60ed:c396:2ff:244:bbd0:9208:26f2:93ea; last_login=2019-11-14 06:14:24; user_agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36;
+[HOLBERTON] user_data INFO 2019-11-19 18:37:59,621: name=***; email=***; phone=***; ssn=***; password=***; ip=f724:c5d1:a14d:c4c5:bae2:9457:3769:1969; last_login=2019-11-14 06:16:19; user_agent=Mozilla/5.0 (Linux; U; Android 4.1.2; de-de; GT-I9100 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30;
+```
