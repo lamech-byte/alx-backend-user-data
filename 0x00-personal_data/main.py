224e1d3425ff454ef430c5b7e3f8ca79b7574774
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-02 15:19:35 +0300

    main.py

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -4,20 +4,9 @@ Main file
 """
 
 import logging
-from filtered_logger import RedactingFormatter
 
-# Create a logger and set its level
-logger = logging.getLogger('my_logger')
-logger.setLevel(logging.INFO)
+get_logger = __import__('filtered_logger').get_logger
+PII_FIELDS = __import__('filtered_logger').PII_FIELDS
 
-# Create a stream handler with the RedactingFormatter
-handler = logging.StreamHandler()
-handler.setFormatter(RedactingFormatter(fields=("email", "ssn", "password")))
-
-# Add the handler to the logger
-logger.addHandler(handler)
-
-message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
-
-# Log the message using the configured logger
-logger.info(message)
+print(get_logger.__annotations__.get('return'))
+print("PII_FIELDS: {}".format(len(PII_FIELDS)))
