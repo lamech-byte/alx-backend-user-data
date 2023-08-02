ef8bd995b9f08b2b6e900fb27377138392690acb
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-02 16:07:02 +0300

    encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,11 +5,13 @@ encrypt_password.py - Module for hashing passwords using bcrypt.
 
 import bcrypt
 
+
 def hash_password(password):
     """ Hash and salt a password using bcrypt """
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
 
 if __name__ == "__main__":
     password = "MyAmazingPassw0rd"
