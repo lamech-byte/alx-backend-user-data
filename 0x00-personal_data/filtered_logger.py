4af4b650a786514a90d47e0b627b8701a5f3ffe8
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-02 15:33:29 +0300

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -35,6 +35,7 @@ class RedactingFormatter(logging.Formatter):
         )
 
 # Define the PII_FIELDS constant containing the fields considered PII.
+
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
