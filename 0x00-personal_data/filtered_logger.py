5ae050ad1612d91aab1b3c83231dddb7caa15ef0
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-02 14:35:49 +0300

    filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+filtered_logger.py - Module for filtering log data using regex.
+"""
+
+import re
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Replace occurrences of certain field values with redaction in the message.
+    """
+    return re.sub(r"(?<=^|{})(?:(?<=\{})[^{}]*(?=\{})|[^{}]*)(?=$|{})".format(
+separator, separator, separator, separator, separator, redaction
+), message)
