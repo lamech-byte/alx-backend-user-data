66e18efc582ed918dabb58c5339e066c1c349233
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-07 16:21:45 +0300

    basic_auth.py

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ BasicAuth module
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -30,4 +31,29 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(' ', 1)[1]
+
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """
+        Decode a Base64 encoded authorization header.
+
+        Args:
+            base64_authorization_header: The Base64 encoded authorization header.
+
+        Returns:
+            The decoded value as a UTF-8 string if valid, otherwise None.
+        """
+        if base64_authorization_header is None or not isinstance(
+            base64_authorization_header, str
+        ):
+            return None
+        
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except:
+            return None
     pass
