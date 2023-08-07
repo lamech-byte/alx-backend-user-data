054bfa245a4973f4f6246067df0c15e3d7a87287
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-07 12:25:26 +0300

    auth.py

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -9,9 +9,23 @@ class Auth:
     """ Auth class for managing API authentication
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Check if authentication is required for a path
         """
-        return False
+        Check if authentication is required for a path.
+
+        Returns:
+            True if path is None
+            True if excluded_paths is None or empty
+            False if path is in excluded_paths
+            True otherwise
+        """
+        if path is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if path == excluded_path or path.startswith(excluded_path):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Get the authorization header from the request
