1ea8852bd194821c2c499482c743973284c0f456
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-07 11:22:04 +0300

    index.py

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort, Blueprint
+from flask import jsonify, Blueprint, abort
+from werkzeug.exceptions import HTTPException
 from api.v1.views import app_views
+
+
+# Create a Blueprint for the views
+app_views = Blueprint("app_views", __name__)
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized_endpoint():
+    # Raise a 401 Unauthorized error using abort with HTTPException
+    raise HTTPException(description="Unauthorized", response=abort(401))
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -24,12 +34,3 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
-
-
-# Create a Blueprint for the views
-app_views = Blueprint("app_views", __name__)
-
-@app_views.route('/api/v1/unauthorized', methods=['GET'])
-def unauthorized_endpoint():
-    # Raise a 401 Unauthorized error using abort
-    abort(401)
