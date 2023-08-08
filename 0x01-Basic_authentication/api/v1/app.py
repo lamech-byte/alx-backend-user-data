b00552d6194a81590e25aa2ed51bade9219fbda9
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-08 16:01:33 +0300

    app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,6 +2,7 @@
 """
 Route module for the API
 """
+
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
@@ -11,12 +12,10 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 import os
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-
 
 if 'AUTH_TYPE' in os.environ:
     if os.environ['AUTH_TYPE'] == 'basic_auth':
@@ -26,16 +25,16 @@ if 'AUTH_TYPE' in os.environ:
         auth = Auth()
 
 
-if 'AUTH_TYPE' in os.environ:
-    if os.environ['AUTH_TYPE'] == 'auth':
-        from api.v1.auth.auth import Auth
-        auth = Auth()
-
 @app.before_request
 def before_request():
+    """
+    Before request handler to perform authentication and authorization checks.
+    """
     if auth:
         required_auth_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-        if request.path not in required_auth_paths and auth.require_auth(request.path, required_auth_paths):
+        if request.path not in required_auth_paths and auth.require_auth(
+            request.path, required_auth_paths
+        ):
             auth_header = auth.authorization_header(request)
             if auth_header is None:
                 abort(401)
@@ -43,29 +42,37 @@ def before_request():
             if current_user is None:
                 abort(403)
 
+
 @app.route('/api/v1/status', methods=['GET'], strict_slashes=False)
 def status():
+    """
+    Status endpoint to return the status of the API.
+    """
     return jsonify({"status": "OK"})
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """
+    Error handler for 404 Not Found.
     """
     return jsonify({"error": "Not found"}), 404
 
 
-# Add the error handler for 401 Unauthorized
 @app.errorhandler(401)
 def unauthorized(error):
+    """
+    Error handler for 401 Unauthorized.
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
-# Add the error handler for 403 Forbidden
 @app.errorhandler(403)
 def forbidden(error):
+    """
+    Error handler for 403 Forbidden.
+    """
     return jsonify({"error": "Forbidden"}), 403
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
