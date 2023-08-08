eaadf7cf86199a943c7ec2fd1927e44690881636
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-08 21:54:21 +0300

    auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -19,11 +19,13 @@ class Auth:
         Returns:
             bool: True if authentication is required, False otherwise.
         """
-        if path is None or excluded_paths is None or not isinstance(excluded_paths, list):
+        if path is None or excluded_paths is None or \
+           not isinstance(excluded_paths, list):
             return True
 
         for excluded_path in excluded_paths:
-            if excluded_path.endswith('*') and path.startswith(excluded_path[:-1]):
+            if excluded_path.endswith('*') and \
+               path.startswith(excluded_path[:-1]):
                 return False
             elif path == excluded_path:
                 return False
