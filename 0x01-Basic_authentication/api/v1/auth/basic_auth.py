d7cd021e128010969ec80819113558b4c4c985da
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-08 19:37:11 +0300

    basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -59,3 +59,29 @@ class BasicAuth(Auth):
             return decoded_string
         except base64.binascii.Error:
             return None
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """
+        Extract user email and password from a decoded Base64 authorization
+        header.
+
+        Args:
+            decoded_base64_authorization_header: The decoded Base64
+            authorization header.
+
+        Returns:
+            A tuple containing the user email and password if valid,
+            otherwise (None, None).
+        """
+        if decoded_base64_authorization_header is None or not isinstance(
+            decoded_base64_authorization_header, str
+        ):
+            return None, None
+        
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        
+        user_email, user_password = decoded_base64_authorization_header.split(':', 1)
+        return user_email, user_password
