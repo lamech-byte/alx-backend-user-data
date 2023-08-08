f9bfb2942305246b075321902efb628b0562b94c
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-08 15:26:13 +0300

    Add files via upload

---
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" Module of Index views
+"""
+from flask import jsonify, Blueprint, abort
+from werkzeug.exceptions import HTTPException, Unauthorized, Forbidden
+from api.v1.views import app_views
+
+
+# Create a Blueprint for the views
+app_views = Blueprint("app_views", __name__)
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized_endpoint():
+    # Raise a 401 Unauthorized error using HTTPException
+    raise Unauthorized(description="Unauthorized")
+
+
+@app_views.route('/api/v1/forbidden', methods=['GET'])
+def forbidden_endpoint():
+    # Raise a 403 Forbidden error using abort
+    abort(403)
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
