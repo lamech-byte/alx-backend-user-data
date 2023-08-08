52413ceed9a8514b8cd40b3f3ba12b7feedd0799
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-08 16:30:29 +0300

    auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,11 +20,14 @@ class Auth:
         """
         if path is None or not excluded_paths:
             return True
-        
+
         for excluded_path in excluded_paths:
-            if path.rstrip('/') == excluded_path.rstrip('/') or path.startswith(excluded_path):
+            if (
+                path.rstrip('/') == excluded_path.rstrip('/') or
+                path.startswith(excluded_path)
+            ):
                 return False
-        
+
         return True
 
     def authorization_header(self, request=None) -> str:
