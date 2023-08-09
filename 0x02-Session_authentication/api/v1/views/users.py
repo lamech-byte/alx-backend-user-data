84a53b24ab17db867d974acdf57b994da56f1a67
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-09 19:14:59 +0300

    users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -120,3 +120,17 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
+
+
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def view_authenticated_user() -> str:
+    """ GET /api/v1/users/me
+    Return:
+      - Authenticated User object JSON represented
+      - 404 if the authenticated User doesn't exist (shouldn't occur)
+    """
+    authenticated_user = request.current_user
+    if authenticated_user is None:
+        abort(404)
+    
+    return jsonify(authenticated_user.to_json())
