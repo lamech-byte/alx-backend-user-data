c1ae6a08180e2152086e57cbea4bf70b085ac1f3
lamech-byte <gesachmoh@gmail.com>
2023-08-09 16:06:36 +0000

    Session_authentication

---
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+""" BasicAuth module
+"""
+
+import base64
+from api.v1.auth.auth import Auth
+from typing import TypeVar
+from models.user import User
+
+
+class BasicAuth(Auth):
+    """ BasicAuth class for managing basic authentication
+    """
+
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        """
+        Extract the Base64 part of the Authorization header for
+        Basic Authentication.
+
+        Args:
+            authorization_header (str): The Authorization header string.
+
+        Returns:
+            str: The Base64 part of the Authorization header if valid,
+            otherwise None.
+        """
+        if authorization_header is None or not isinstance(
+            authorization_header, str
+        ):
+            return None
+
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header.split(' ', 1)[1]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """
+        Decode a Base64 encoded authorization header.
+
+        Args:
+            base64_authorization_header (str): The Base64 encoded
+            authorization header.
+
+        Returns:
+            str: The decoded value as a UTF-8 string if valid,
+            otherwise None.
+        """
+        if base64_authorization_header is None or not isinstance(
+            base64_authorization_header, str
+        ):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except base64.binascii.Error:
+            return None
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """
+        Extract user email and password from a decoded Base64 authorization
+        header.
+
+        Args:
+            decoded_base64_authorization_header: The decoded Base64
+            authorization header.
+
+        Returns:
+            A tuple containing the user email and password if valid,
+            otherwise (None, None).
+        """
+        if decoded_base64_authorization_header is None or not isinstance(
+            decoded_base64_authorization_header, str
+        ):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        user_credentials = decoded_base64_authorization_header.rsplit(':', 1)
+        print(f"User credentials: {user_credentials}")
+        if len(user_credentials) != 2:
+            return None, None
+
+        user_email, user_password = user_credentials
+        return user_email, user_password
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
+        """
+        Get the User instance based on user email and password.
+
+        Args:
+            user_email (str): The user's email.
+            user_pwd (str): The user's password.
+
+        Returns:
+            User: The User instance if found and credentials are valid,
+            otherwise None.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
---
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Module of Index views
+"""
+
+from flask import jsonify, Blueprint, abort
+from werkzeug.exceptions import HTTPException, Unauthorized, Forbidden
+from api.v1.views import app_views
+
+# Create a Blueprint for the views
+app_views = Blueprint("app_views", __name__)
+
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized_endpoint():
+    """
+    Endpoint to raise a 401 Unauthorized error using HTTPException.
+    """
+    raise Unauthorized(description="Unauthorized")
+
+
+@app_views.route('/api/v1/forbidden', methods=['GET'])
+def forbidden_endpoint():
+    """
+    Endpoint to raise a 403 Forbidden error using abort.
+    """
+    abort(403)
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """
+    GET /api/v1/status
+    Returns:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"}), 200
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """
+    GET /api/v1/stats
+    Returns:
+      - the number of each object
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats), 200
