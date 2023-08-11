e8daa885d9677af02ab16512f1fd5af5012eb3b5
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-11 17:43:56 +0300

    session_exp_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -93,7 +93,9 @@ class SessionExpAuth(SessionAuth):
                 user = User.get(user_id)
                 return user
 
-    @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+    @app_views.route(
+        '/auth_session/login', methods=['POST'], strict_slashes=False
+    )
     def session_login():
         email = request.form.get('email')
         password = request.form.get('password')
@@ -102,7 +104,7 @@ class SessionExpAuth(SessionAuth):
             return jsonify({"error": "email missing"}), 400
         if not password:
             return jsonify({"error": "password missing"}), 400
-            
+     
             user = User.search({"email": email})
         if not user:
             return jsonify({"error": "no user found for this email"}), 404
@@ -113,7 +115,9 @@ class SessionExpAuth(SessionAuth):
         session_id = auth.create_session(user[0].id)
         user_json = user[0].to_json()
         response = jsonify(user_json)
-        response.set_cookie(os.environ.get('SESSION_NAME', '_my_session_id'), session_id)
+        response.set_cookie(os.environ.get(
+            'SESSION_NAME', '_my_session_id'
+        ), session_id)
         return response
-        
+
         return None
