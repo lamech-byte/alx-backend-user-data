7dfd2b8d01a86074c371d7a0c135190117e7dd2c
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-11 18:15:13 +0300

    __init__.py

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -3,6 +3,7 @@
 """
 from flask import Blueprint
 from api.v1.views import session_auth
+from api.v1.views.session_auth import app_views as session_auth_views
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 app_views.register_blueprint(session_auth.app_views)
