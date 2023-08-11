85849268babee3f6dd3a25de828efae024ad782b
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-11 17:25:44 +0300

    session_exp_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -36,7 +36,9 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
-    def user_id_for_session_id(self, session_id: str = None) -> TypeVar('User'):
+    def user_id_for_session_id(
+        self, session_id: str = None
+    ) -> TypeVar('User'):
         """
         Retrieves a user ID based on a Session ID.
 
