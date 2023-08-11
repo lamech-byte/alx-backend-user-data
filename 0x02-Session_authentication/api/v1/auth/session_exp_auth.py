30c9b84312bb670786a28ab1587678aca622706b
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-11 08:33:02 +0300

    session_exp_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -3,13 +3,14 @@
 SessionExpAuth module
 """
 
-from datetime import datetime, timedelta
 from api.v1.auth.session_auth import SessionAuth
+from typing import List, TypeVar
+from datetime import datetime, timedelta
 
 
 class SessionExpAuth(SessionAuth):
     """
-    SessionExpAuth class for managing session-based authentication with expiration
+    SessionExpAuth class for managing Session authentication with expiration
     """
 
     def __init__(self):
@@ -17,21 +18,17 @@ class SessionExpAuth(SessionAuth):
         Constructor
         """
         super().__init__()
-        session_duration = os.environ.get('SESSION_DURATION')
-        try:
-            self.session_duration = int(session_duration)
-        except (ValueError, TypeError):
-            self.session_duration = 0
+        self.session_duration = int(os.environ.get('SESSION_DURATION', 0))
 
     def create_session(self, user_id=None):
         """
-        Creates a Session ID with expiration
+        Creates a new session for a user.
 
         Args:
             user_id (str): The user ID.
 
         Returns:
-            str: The created Session ID.
+            str: The new session ID.
         """
         session_id = super().create_session(user_id)
         if session_id is None:
@@ -46,31 +43,27 @@ class SessionExpAuth(SessionAuth):
 
     def user_id_for_session_id(self, session_id=None):
         """
-        Retrieves the User ID for a given Session ID, with session expiration.
+        Retrieves a user ID based on a session ID.
 
         Args:
-            session_id (str): The Session ID.
+            session_id (str): The session ID.
 
         Returns:
-            str: The User ID associated with the Session ID if valid and not expired, None otherwise.
+            str: The associated user ID, or None if not found or expired.
         """
-        if session_id is None:
+        if session_id is None or session_id not in self.user_id_by_session_id:
             return None
 
-        session_dict = self.user_id_by_session_id.get(session_id)
-        if session_dict is None:
-            return None
+        session_dict = self.user_id_by_session_id[session_id]
+        if self.session_duration > 0:
+            created_at = session_dict.get('created_at')
+            if created_at is None:
+                return None
 
-        user_id = session_dict.get('user_id')
-        if self.session_duration <= 0:
-            return user_id
+            expiration_time = created_at + timedelta(
+                seconds=self.session_duration
+            )
+            if expiration_time < datetime.now():
+                return None
 
-        created_at = session_dict.get('created_at')
-        if created_at is None:
-            return None
-
-        expiration_time = created_at + timedelta(seconds=self.session_duration)
-        if datetime.now() > expiration_time:
-            return None
-
-        return user_id
+        return session_dict.get('user_id')
