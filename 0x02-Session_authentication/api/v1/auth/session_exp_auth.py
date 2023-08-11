02741aa5398bb3df13b6d428200c126c49ea1c00
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-11 17:29:00 +0300

    session_exp_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -5,7 +5,6 @@ SessionExpAuth module
 
 from api.v1.auth.session_auth import SessionAuth
 from datetime import datetime, timedelta
-from api.v1.auth.auth import Auth
 import uuid
 from models.user import User
 from typing import TypeVar
@@ -13,13 +12,24 @@ from flask import request
 import os
 
 
-class SessionAuth(Auth):
+class SessionExpAuth(SessionAuth):
     """
-    SessionAuth class for managing session-based authentication.
+    SessionExpAuth class for managing session-based authentication
+    with session expiration.
     """
-    user_id_by_session_id = {}
 
-    def create_session(self, user_id: str = None) -> str:
+    def __init__(self):
+        """
+        Constructor
+        """
+        super().__init__()
+        session_duration_str = os.environ.get('SESSION_DURATION', '0')
+        try:
+            self.session_duration = int(session_duration_str)
+        except ValueError:
+            self.session_duration = 0
+
+    def create_session(self, user_id=None):
         """
         Creates a Session ID for a user ID.
 
@@ -29,16 +39,18 @@ class SessionAuth(Auth):
         Returns:
             str: The newly created Session ID.
         """
-        if user_id is None:
-            return None
+        session_id = super().create_session(user_id)
+        if session_id:
+            if self.user_id_by_session_id is None:
+                self.user_id_by_session_id = {}
+            self.user_id_by_session_id[session_id] = {
+                "user_id": user_id,
+                "created_at": datetime.now()
+            }
+            return session_id
+        return None
 
-        session_id = str(uuid.uuid4())
-        self.user_id_by_session_id[session_id] = user_id
-        return session_id
-
-    def user_id_for_session_id(
-        self, session_id: str = None
-    ) -> TypeVar('User'):
+    def user_id_for_session_id(self, session_id=None):
         """
         Retrieves a user ID based on a Session ID.
 
@@ -46,12 +58,22 @@ class SessionAuth(Auth):
             session_id (str): The Session ID.
 
         Returns:
-            str: The user ID if found, None otherwise.
+            str: The user ID if found and session is not expired,
+            None otherwise.
         """
-        if session_id is None or session_id not in self.user_id_by_session_id:
+        if session_id is None or self.user_id_by_session_id is None:
             return None
 
-        return self.user_id_by_session_id.get(session_id)
+        session_data = self.user_id_by_session_id.get(session_id)
+        if session_data:
+            created_at = session_data.get('created_at')
+            if created_at:
+                current_time = datetime.now()
+                expiration_time = created_at + timedelta(
+                    seconds=self.session_duration
+                )
+                if current_time <= expiration_time:
+                    return session_data.get('user_id')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
