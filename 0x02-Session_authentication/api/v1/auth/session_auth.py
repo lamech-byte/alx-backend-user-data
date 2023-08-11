7f7639d8e2adc829673ac0b95b3cd6d526a404bc
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-11 18:05:00 +0300

    session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,12 @@ SessionAuth module
 
 import uuid
 from api.v1.auth.auth import Auth
+from flask import Blueprint, Flask, request, jsonify
+from api.v1.views import app_views
+from models.user import User
+
+
+app_views = Blueprint('session_auth', __name__, url_prefix='/api/v1/auth_session')
 
 
 class SessionAuth(Auth):
@@ -43,5 +49,32 @@ class SessionAuth(Auth):
         """
         if session_id is None or not isinstance(session_id, str):
             return None
+
+    @app_views.route(
+        '/auth_session/login', methods=['POST'], strict_slashes=False
+    )
+    def session_login():
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if not email:
+            return jsonify({"error": "email missing"}), 400
+        if not password:
+            return jsonify({"error": "password missing"}), 400
+
+            user = User.search({"email": email})
+        if not user:
+            return jsonify({"error": "no user found for this email"}), 404
+
+        if not user[0].is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+
+        session_id = auth.create_session(user[0].id)
+        user_json = user[0].to_json()
+        response = jsonify(user_json)
+        response.set_cookie(os.environ.get(
+            'SESSION_NAME', '_my_session_id'
+        ), session_id)
+        return response
 
         return self.user_id_by_session_id.get(session_id)
