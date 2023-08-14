72bbbeec0a9daf1091013da63ad068edca20c5fd
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-14 18:59:40 +0300

    session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -14,6 +14,7 @@ from api.v1.views import app_views
 from models.user import User
 from api.v1.app import auth
 
+
 @app_views.route(
   '/auth_session/login', methods=['POST'], strict_slashes=False
 )
