608df88387128fcdedbfabe7cac2de93106610b7
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-14 20:57:52 +0300

    user_session.py

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -2,22 +2,25 @@
 """
 UserSession Module
 
-This module defines the UserSession class, which represents user session information stored in the database.
+This module defines the UserSession class, which represents user
+session information stored in the database.
 """
 
 from models.base import Base
 from sqlalchemy import Column, String
 
+
 class UserSession(Base):
     """
     UserSession class
 
-    This class defines the UserSession model with attributes user_id and session_id.
+    This class defines the UserSession model with attributes
+    user_id and session_id.
     """
     __tablename__ = 'user_sessions'
     user_id = Column(String(60), nullable=False)
     session_id = Column(String(60), nullable=False)
-    
+
     def __init__(self, *args: list, **kwargs: dict):
         """
         Initializes UserSession instance.
