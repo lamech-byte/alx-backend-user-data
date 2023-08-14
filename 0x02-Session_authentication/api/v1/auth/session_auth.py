0e1c727a362d3b78b5f1c8739e6113ef3271b92e
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-14 14:29:03 +0300

    session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,6 +9,23 @@ from api.v1.auth.auth import Auth
 class SessionAuth(Auth):
     """
     SessionAuth class for managing session-based authentication
-    (Initial version with no methods)
     """
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user_id.
+
+        Args:
+            user_id (str): The user ID.
+
+        Returns:
+            str: The created Session ID or None.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
