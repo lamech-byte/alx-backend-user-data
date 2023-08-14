f031148bbbf8de0da9f8e05716e2124d5fc6054e
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-14 12:14:00 +0300

    Update __init__.py

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,10 +1,1 @@
-#!/usr/bin/env python3
-""" DocDocDocDocDocDoc
-"""
-from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
-from api.v1.views import index, users, session_auth
-
-User.load_from_file()
