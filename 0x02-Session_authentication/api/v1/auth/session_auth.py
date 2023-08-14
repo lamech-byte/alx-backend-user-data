1eb15b2c336a1b6922959ba3f458242d0915277b
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-14 14:10:13 +0300

    session_auth.py

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+SessionAuth module
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    SessionAuth class for managing session-based authentication
+    (Initial version with no methods)
+    """
+    pass
