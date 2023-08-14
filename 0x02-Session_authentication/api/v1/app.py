c49f7cc7f27eabd1243cccf8ec119cdd195262ed
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-14 20:52:07 +0300

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -12,6 +12,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 import os
 
 app = Flask(__name__)
@@ -29,6 +30,8 @@ if 'AUTH_TYPE' in os.environ:
         auth = SessionAuth()
     elif auth_type == "session_exp_auth":
         auth = SessionExpAuth()
+    elif auth_type == 'session_db_auth':
+        auth = SessionDBAuth()
     else:
         auth = Auth()
 
