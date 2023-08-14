a6a648ecb3f9bf5c9793cdc52912afbaa157e2ae
lamech-byte <gesachmoh@gmail.com>
2023-08-14 10:28:32 +0000

    backend-user-data

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+""" Auth module
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """ Auth class for managing API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determine if authentication is required for a given path.
+
+        Args:
+            path (str): The path to be checked.
+            excluded_paths (list of str): List of paths to be excluded.
+
+        Returns:
+            bool: True if authentication is required, False otherwise.
+        """
+        if path is None or excluded_paths is None or \
+           not isinstance(excluded_paths, list):
+            return True
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*') and \
+               path.startswith(excluded_path[:-1]):
+                return False
+            elif path == excluded_path:
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Get the authorization header from the request.
+
+        Returns:
+            The value of the header 'Authorization' if present,
+            None otherwise.
+        """
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Get the current user from the request
+        """
+        return None
