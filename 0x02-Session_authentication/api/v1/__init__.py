0d819bbe27ecfabb84c74d7fe39a44d38646e579
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-14 12:44:33 +0300

    Update __init__.py

--- 0x02-Session_authentication/api/v1/__init__.py
+++ 0x02-Session_authentication/api/v1/__init__.py
@@ -3,9 +3,8 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # Import views
-from api.v1.views import index, users, session_auth
+from api.v1.views import index, users
 
 # Register the blueprints
 app_views.register_blueprint(index.index)
 app_views.register_blueprint(users.users)
-app_views.register_blueprint(session_auth.session_auth)
