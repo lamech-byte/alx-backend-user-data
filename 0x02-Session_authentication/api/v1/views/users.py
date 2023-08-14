5c65128924bfea0eb1cf1b7a39ffd5b6355056a7
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-14 12:40:41 +0300

    Update users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module of Users views
 """
-from api.v1.views import app_views
+from api.v1 import app_views
 from flask import Flask, jsonify, abort, request
 from models.user import User
 from api.v1.auth import auth
