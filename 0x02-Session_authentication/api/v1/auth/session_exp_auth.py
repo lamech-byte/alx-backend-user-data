77e51b58b5867a34249c443fca25f19765381ebe
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-14 19:58:23 +0300

    session_exp_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -10,11 +10,13 @@ from api.v1.auth.session_auth import SessionAuth
 from datetime import datetime, timedelta
 import os
 
+
 class SessionExpAuth(SessionAuth):
     """
     SessionExpAuth Class
 
-    This class inherits from SessionAuth and adds the functionality of session expiration
+    This class inherits from SessionAuth and adds the functionality
+    of session expiration
     to the session-based authentication mechanism.
     """
 
@@ -23,8 +25,10 @@ class SessionExpAuth(SessionAuth):
         Initialize SessionExpAuth instance.
 
         This constructor overloads the parent constructor and also initializes
-        the session duration based on the SESSION_DURATION environment variable.
-        If the environment variable is missing or invalid, a default of 0 is used.
+        the session duration based on the SESSION_DURATION environment
+        variable.
+        If the environment variable is missing or invalid, a default
+        of 0 is used.
 
         Args:
             None
@@ -43,11 +47,13 @@ class SessionExpAuth(SessionAuth):
         """
         Create a session for the given user.
 
-        This method overloads the parent method and adds the session creation time
+        This method overloads the parent method and adds the session
+        creation time
         to the user_id_by_session_id dictionary.
 
         Args:
-            user_id (str): The ID of the user for whom the session is being created.
+            user_id (str): The ID of the user for whom the session
+            is being created.
 
         Returns:
             str: The created session ID.
@@ -71,7 +77,8 @@ class SessionExpAuth(SessionAuth):
             session_id (str): The session ID to look up.
 
         Returns:
-            str or None: The associated user ID or None if the session is invalid.
+            str or None: The associated user ID or None if the
+            session is invalid.
         """
         if not session_id or session_id not in self.user_id_by_session_id:
             return None
