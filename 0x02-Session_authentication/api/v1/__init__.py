9efdf9977a12e09ed8759a727463d74d8bf1df8b
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-14 12:30:25 +0300

    __init__.py

--- 0x02-Session_authentication/api/v1/__init__.py
+++ 0x02-Session_authentication/api/v1/__init__.py
@@ -4,3 +4,8 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # Import views
 from api.v1.views import index, users, session_auth
+
+# Register the blueprints
+app_views.register_blueprint(index.index)
+app_views.register_blueprint(users.users)
+app_views.register_blueprint(session_auth.session_auth)
