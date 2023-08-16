cc1871a3aec9755548a5197c7ea74b09def23d55
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-16 13:59:09 +0300

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,59 +1,20 @@
-#!/usr/bin/env python3
 """auth module
 """
-
-from db import DB
-from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
-from typing import Union
-from user import User
 
-
-class Auth:
-    """Auth class to interact with the authentication database.
+def _hash_password(password: str) -> bytes:
     """
+    Hash a password using bcrypt.
 
-    def __init__(self) -> None:
-        """Initialize a new Auth instance.
-        """
-        self._db = DB()
+    Args:
+        password (str): The password string to hash.
 
-    def _hash_password(self, password: str) -> bytes:
-        """
-        Hash a password using bcrypt.
-
-        Args:
-            password (str): The password string to hash.
-
-        Returns:
-            bytes: The hashed password bytes.
-        """
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-        return hashed_password
-
-    def register_user(self, email: str, password: str) -> Union[None, User]:
-        """
-        Register a new user.
-
-        Args:
-            email (str): The user's email.
-            password (str): The user's password.
-
-        Returns:
-            Union[None, User]: The created User object.
-
-        Raises:
-            ValueError: If a user with the same email already exists.
-        """
-        try:
-            existing_user = self._db.find_user_by(email=email)
-            raise ValueError(f"User {email} already exists")
-        except NoResultFound:
-            hashed_password = self._hash_password(password)
-            user = self._db.add_user(email, hashed_password)
-            return user
-
+    Returns:
+        bytes: The hashed password bytes.
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
 
 if __name__ == "__main__":
-    print(Auth()._hash_password("Hello Holberton"))
+    print(_hash_password("Hello Holberton"))
