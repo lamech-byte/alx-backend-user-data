18332df0bfdba5d5577da403f183b0464cd5ec2e
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-16 17:52:05 +0300

    main.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,15 +2,11 @@
 """
 Main file
 """
-
 from auth import Auth
 
-email = 'bob@bob.com'
-password = 'MyPwdOfBob'
 auth = Auth()
+email = 'test@test.com'
+password = 'Hello Holberton'
 
-auth.register_user(email, password)
-
-print(auth.valid_login(email, password))
-print(auth.valid_login(email, "WrongPwd"))
-print(auth.valid_login("unknown@email", password))
+hashed_password = auth._hash_password(password)
+print(hashed_password)
