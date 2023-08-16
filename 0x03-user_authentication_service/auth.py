4577fcae7fbed7df3e517de120653a97f3067046
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-16 08:51:46 +0300

    auth.py

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""auth module
+"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hash a password using bcrypt.
+
+    Args:
+        password (str): The password string to hash.
+
+    Returns:
+        bytes: The hashed password bytes.
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+if __name__ == "__main__":
+    print(_hash_password("Hello Holberton"))
