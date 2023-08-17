139023afc79c90d1f9ebf3c26a17abedc4212f3f
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-17 08:12:49 +0300

    main.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -11,5 +11,7 @@ auth = Auth()
 auth.register_user(email, password)
 
 print(auth.valid_login(email, password))
+
 print(auth.valid_login(email, "WrongPwd"))
+
 print(auth.valid_login("unknown@email", password))
