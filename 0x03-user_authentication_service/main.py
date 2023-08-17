b2e9b472a57f1882f1fcbd2e989b6fb4be11c32d
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-17 11:04:54 +0300

    main.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,6 +2,7 @@ import requests
 
 BASE_URL = "http://localhost:5000"
 
+
 def register_user(email: str, password: str) -> None:
     response = requests.post(
         f"{BASE_URL}/users",
@@ -9,6 +10,7 @@ def register_user(email: str, password: str) -> None:
     )
     assert response.status_code == 201
     print("User registered successfully.")
+
 
 def log_in_wrong_password(email: str, password: str) -> None:
     response = requests.post(
@@ -18,10 +20,12 @@ def log_in_wrong_password(email: str, password: str) -> None:
     assert response.status_code == 401
     print("Incorrect password login failed.")
 
+
 def profile_unlogged() -> None:
     response = requests.get(f"{BASE_URL}/profile")
     assert response.status_code == 403
     print("Profile access without login failed.")
+
 
 def log_in(email: str, password: str) -> str:
     response = requests.post(
@@ -33,6 +37,7 @@ def log_in(email: str, password: str) -> str:
     print("Logged in successfully.")
     return session_id
 
+
 def profile_logged(session_id: str) -> None:
     headers = {"Cookie": f"session_id={session_id}"}
     response = requests.get(f"{BASE_URL}/profile", headers=headers)
@@ -40,11 +45,13 @@ def profile_logged(session_id: str) -> None:
     email = response.json().get("email")
     print(f"Profile of user {email} accessed successfully.")
 
+
 def log_out(session_id: str) -> None:
     headers = {"Cookie": f"session_id={session_id}"}
     response = requests.delete(f"{BASE_URL}/sessions", headers=headers)
     assert response.status_code == 302
     print("Logged out successfully.")
+
 
 def reset_password_token(email: str) -> str:
     response = requests.post(
@@ -56,13 +63,18 @@ def reset_password_token(email: str) -> str:
     print(f"Reset password token generated: {reset_token}")
     return reset_token
 
+
 def update_password(email: str, reset_token: str, new_password: str) -> None:
     response = requests.put(
         f"{BASE_URL}/reset_password",
-        data={"email": email, "reset_token": reset_token, "new_password": new_password}
+        data={
+            "email": email,
+            "reset_token": reset_token,
+            "new_password": new_password}
     )
     assert response.status_code == 200
     print("Password updated successfully.")
+
 
 EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
